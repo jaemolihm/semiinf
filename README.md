806389ff33c244ab86bd51083ce5dbad0bc553d6
JaemoLim <jaemolim96@gmail.com>
2018-10-28 20:57:01 +0900

    Update util_match so that input parameters are written in a seperate file

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## How to use semiinf
 1. Edit Makefile and compile semiinf
 	* make clean; make
-2. (If bulk-slab matching is required) Edit input parameters in util_match/match_main.py and run it.
+2. (If bulk-slab matching is required) Edit input parameters in util_match/match_input.py and run.
 	* python3 util_match/match_main.py
 3. Edit input file and do semiinfinite calculation
 	* ./semiinf.x input_file.in
--- util_match/match_main.py
+++ util_match/match_main.py
@@ -4,6 +4,7 @@ import numpy as np
 import numpy.matlib
 import scipy.linalg
 
+from match_input import setup_input
 from match_module_read_data import (tb_read_data, tb_sort_wcenter, tb_shift_fermi,
     tb_correct_data)
 from match_module_ham_process import run_opt_diagonly
@@ -13,31 +14,7 @@ from match_module_ham_mismatch import (ham_mismatch_intra, ham_mismatch_inter,
 
 ################
 # input parameters #
-material = 'GeTe'
-path = './'
-input_params = {}
-
-input_params['seedname_b'] = 'GeTe.bulk'
-input_params['seedname_s'] = 'GeTe.slab'
-input_params['isspinor'] = True
-
-input_params['iatm_add'] = 4
-input_params['nbnd_s'] = 310
-input_params['nw_s'] = 248
-input_params['nklist_s'] = np.array([7,7,1])
-input_params['nbnd_b'] = 60
-input_params['nw_b'] = 48
-input_params['nklist_b'] = np.array([7,7,6])
-input_params['iw_head'] = 104
-input_params['iw_tail'] = 152
-input_params['norbital_list'] = [8]*6
-
-input_params['alat_s'] = np.array([[ 4.177748, 0.000000,  0.000000],
-                   [-2.088874, 3.618036,  0.000000],
-                   [ 0.000000, 0.000000, 62.869262]]).transpose()
-input_params['alat_b'] = np.array([[ 4.177748, 0.000000,  0.000000],
-                   [-2.088874, 3.618036,  0.000000],
-                   [ 0.000000, 0.000000, 10.478210]]).transpose()
+material, path, input_params = setup_input()
 ################################
 # read binary files and initialize them
 ################################

3ca6778c0dd36969372ada31235c62402a1894a0
Jae-Mo Lihm <jaemolim96@gmail.com>
2019-10-03 19:53:40 +0900

    Clean main.f90, add preprocessing flag for MPI.

--- src/comms.f90
+++ src/comms.f90
@@ -24,127 +24,160 @@ MODULE comms
 !! Common variables and subroutines used in semiinf.x
 !------------------------------------------------------------------------
   USE parallel_include
+  !
   IMPLICIT NONE
+  !
   SAVE
+  !
   INTEGER, PARAMETER :: DP = selected_real_kind(14, 200)
   REAL(DP), PARAMETER :: pi = 3.141592653589793238462643383279502884197169399375105820974944_dp
   COMPLEX(DP), PARAMETER :: ci = (0.d0, 1.d0)
   COMPLEX(DP), PARAMETER :: cone = (1.d0, 0.d0)
   COMPLEX(DP), PARAMETER :: czero = (0.d0, 0.d0)
   !
-  ! variables for mpi
+  ! MPI variables
   INTEGER, PARAMETER :: root_process = 0
-  LOGICAL :: is_root
-  INTEGER :: my_id, num_procs
-  !
+  LOGICAL :: is_root = .true.
+  !! true if this processor is rooot (my_id == 0)
+  INTEGER :: num_procs = 1
+  !! Number of total processors
+  INTEGER :: my_id = 0
+  !! Processor index (from 0 to num_procs - 1)
+  INTEGER :: world_comm = 0
+  !! communicator
 CONTAINS
+!
+!--------------------------------------------------------------------------
+SUBROUTINE mp_setup()
+  IMPLICIT NONE
+  INTEGER :: ierr
+#if defined (__MPI)
+  CALL MPI_INIT(ierr)
+  CALL MPI_COMM_RANK(MPI_COMM_WORLD, my_id, ierr)
+  CALL MPI_COMM_SIZE(MPI_COMM_WORLD, num_procs, ierr)
+  is_root = .false.
+  IF (my_id == root_process) is_root = .true.
+  world_comm = MPI_COMM_WORLD
+#endif
+END SUBROUTINE mp_setup
+!--------------------------------------------------------------------------
+!
+!--------------------------------------------------------------------------
+SUBROUTINE mp_end()
+  IMPLICIT NONE
+  INTEGER :: ierr
+#if defined (__MPI)
+  CALL MPI_FINALIZE(ierr)
+#endif
+END SUBROUTINE mp_end
+!--------------------------------------------------------------------------
+!
+!--------------------------------------------------------------------------
+SUBROUTINE mp_barrier(gid)
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: gid
+  INTEGER :: ierr
+#if defined (__MPI)
+  CALL MPI_BARRIER(gid, ierr)
+  IF (ierr /= 0) CALL MPI_ABORT(MPI_COMM_WORLD, 8066, ierr)
+#endif
+END SUBROUTINE mp_barrier
+!--------------------------------------------------------------------------
+!
+!--------------------------------------------------------------------------
+SUBROUTINE io_error(error_msg)
+  IMPLICIT NONE
+  CHARACTER(LEN=*), INTENT(IN) :: error_msg
+  WRITE(*,*) error_msg
+  STOP
+END SUBROUTINE io_error
+!--------------------------------------------------------------------------
+!
+! Remains only due to legacy. Not used in code.
+! !------------------------------------------------------------------------
+! SUBROUTINE linspace(out_list, start, end, ind_start, ind_end)
+! !------------------------------------------------------------------------
+! !! mimic linspace function of python numpy
+! !------------------------------------------------------------------------
+!   REAL(DP), INTENT(IN) :: start, end
+!   INTEGER, INTENT(IN) :: ind_start, ind_end
+!   REAL(DP), INTENT(OUT) :: out_list(:)
+!   !
+!   REAL(DP) :: value, step
+!   INTEGER :: i
+!   step = (end - start) / (ind_end - ind_start)
+!   value = start
+!   DO i = ind_start, ind_end
+!       out_list(i) = value
+!       value = value + step
+!   END DO
+! END SUBROUTINE linspace
+!
+!------------------------------------------------------------------------
+SUBROUTINE inv_omega_minus_mat(ndim, mat_in, omega, mat_out, flag)
+!------------------------------------------------------------------------
+!! Compute mat_out = (omega - mat_in)^-1
+!------------------------------------------------------------------------
+  INTEGER, INTENT(IN) :: ndim
+  COMPLEX(DP), INTENT(IN) :: omega
+  COMPLEX(DP), INTENT(IN) :: mat_in(ndim, ndim)
+  COMPLEX(DP), INTENT(OUT) :: mat_out(ndim, ndim)
+  CHARACTER(LEN=*), INTENT(IN) :: flag
+  !! Description to be included in the error message when inversion fails
   !
-  SUBROUTINE mpi_setup()
-    IMPLICIT NONE
-    INTEGER :: ierr
-    CALL MPI_INIT(ierr)
-    CALL MPI_COMM_RANK(MPI_COMM_WORLD, my_id, ierr)
-    CALL MPI_COMM_SIZE(MPI_COMM_WORLD, num_procs, ierr)
-    is_root = .false.
-    IF (my_id == root_process) is_root = .true.
-  END SUBROUTINE mpi_setup
+  COMPLEX(DP), ALLOCATABLE :: mat_temp(:, :)
+  INTEGER, ALLOCATABLE :: ipiv(:)
+  INTEGER :: i
+  INTEGER :: info
   !
-  SUBROUTINE mpi_end()
-    IMPLICIT NONE
-    INTEGER :: ierr
-    CALL MPI_FINALIZE(ierr)
-  END SUBROUTINE mpi_end
+  ! Set mat_temp = omega - mat_in
+  ALLOCATE(mat_temp(ndim, ndim))
+  mat_temp = - mat_in
+  mat_out = czero
+  DO i = 1, ndim
+      mat_temp(i, i) = omega + mat_temp(i, i)
+      mat_out(i, i) = cone
+  END DO
   !
-  SUBROUTINE io_error(error_msg)
-    IMPLICIT NONE
-    CHARACTER(LEN=*), INTENT(IN) :: error_msg
-    WRITE(*,*) error_msg
-    STOP
-  END SUBROUTINE io_error
+  ! Invert matrix
+  ALLOCATE(ipiv(ndim))
+  CALL ZGESV(ndim,ndim, mat_temp, ndim, ipiv, mat_out, ndim, info)
+  IF (info /= 0) CALL io_error('Error in ZGESV: matinv inversion in ' // flag)
   !
-  ! Remains only due to legacy. Not used in code.
-  ! !------------------------------------------------------------------------
-  ! SUBROUTINE linspace(out_list, start, end, ind_start, ind_end)
-  ! !------------------------------------------------------------------------
-  ! !! mimic linspace function of python numpy
-  ! !------------------------------------------------------------------------
-  !   REAL(DP), INTENT(IN) :: start, end
-  !   INTEGER, INTENT(IN) :: ind_start, ind_end
-  !   REAL(DP), INTENT(OUT) :: out_list(:)
-  !   !
-  !   REAL(DP) :: value, step
-  !   INTEGER :: i
-  !   step = (end - start) / (ind_end - ind_start)
-  !   value = start
-  !   DO i = ind_start, ind_end
-  !       out_list(i) = value
-  !       value = value + step
-  !   END DO
-  ! END SUBROUTINE linspace
+  DEALLOCATE(mat_temp)
+  DEALLOCATE(ipiv)
+END SUBROUTINE inv_omega_minus_mat
+!
+!------------------------------------------------------------------------
+SUBROUTINE k_operator(nrpts, oper_r, rvec, ndegen, kx, ky, oper_k)
+!------------------------------------------------------------------------
+!! Compute fourier transform of an operator
+!! oper_k(:,:) = exp(1j*k*rvec) * oper_r(:,:,ir) / ndegen(ir)
+!------------------------------------------------------------------------
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: nrpts
+  REAL(DP), INTENT(IN) :: kx, ky
+  COMPLEX(DP), INTENT(IN) :: oper_r(:, :, :)
+  REAL(DP), INTENT(IN) :: rvec(:, :)
+  REAL(DP), INTENT(IN) :: ndegen(:)
+  COMPLEX(DP), INTENT(OUT) :: oper_k(:, :)
   !
-  !------------------------------------------------------------------------
-  SUBROUTINE inv_omega_minus_mat(ndim, mat_in, omega, mat_out, flag)
-  !------------------------------------------------------------------------
-  !! Compute mat_out = (omega - mat_in)^-1
-  !------------------------------------------------------------------------
-    INTEGER, INTENT(IN) :: ndim
-    COMPLEX(DP), INTENT(IN) :: omega
-    COMPLEX(DP), INTENT(IN) :: mat_in(ndim, ndim)
-    COMPLEX(DP), INTENT(OUT) :: mat_out(ndim, ndim)
-    CHARACTER(LEN=*), INTENT(IN) :: flag
-    !! Description to be included in the error message when inversion fails
-    !
-    COMPLEX(DP), ALLOCATABLE :: mat_temp(:, :)
-    INTEGER, ALLOCATABLE :: ipiv(:)
-    INTEGER :: i
-    INTEGER :: info
-    !
-    ! Set mat_temp = omega - mat_in
-    ALLOCATE(mat_temp(ndim, ndim))
-    mat_temp = - mat_in
-    mat_out = czero
-    DO i = 1, ndim
-        mat_temp(i, i) = omega + mat_temp(i, i)
-        mat_out(i, i) = cone
-    END DO
-    !
-    ! Invert matrix
-    ALLOCATE(ipiv(ndim))
-    CALL ZGESV(ndim,ndim, mat_temp, ndim, ipiv, mat_out, ndim, info)
-    IF (info /= 0) CALL io_error('Error in ZGESV: matinv inversion in ' // flag)
-    !
-    DEALLOCATE(mat_temp)
-    DEALLOCATE(ipiv)
-  END SUBROUTINE inv_omega_minus_mat
+  INTEGER :: ir
+  COMPLEX(DP) :: coeff_ik
   !
-  !------------------------------------------------------------------------
-  SUBROUTINE k_operator(nrpts, oper_r, rvec, ndegen, kx, ky, oper_k)
-  !------------------------------------------------------------------------
-  !! Compute fourier transform of an operator
-  !! oper_k(:,:) = exp(1j*k*rvec) * oper_r(:,:,ir) / ndegen(ir)
-  !------------------------------------------------------------------------
-    IMPLICIT NONE
-    INTEGER, INTENT(IN) :: nrpts
-    REAL(DP), INTENT(IN) :: kx, ky
-    COMPLEX(DP), INTENT(IN) :: oper_r(:, :, :)
-    REAL(DP), INTENT(IN) :: rvec(:, :)
-    REAL(DP), INTENT(IN) :: ndegen(:)
-    COMPLEX(DP), INTENT(OUT) :: oper_k(:, :)
-    !
-    INTEGER :: ir
-    COMPLEX(DP) :: coeff_ik
-    !
-    oper_k = czero
-    DO ir = 1, nrpts
-        coeff_ik = EXP(ci * 2.d0 * pi * (kx * rvec(1, ir) + ky * rvec(2, ir)))
-        coeff_ik = coeff_ik / ndegen(ir)
-        oper_k = oper_k + oper_r(:,:,ir) * coeff_ik
-    ENDDO
-  END SUBROUTINE k_operator
-  !
-  !--------------------------------------------------------------------------
-  FUNCTION find_free_unit()
-  !! Taken from QE/UtilXlib/find_free_unit.f90
+  oper_k = czero
+  DO ir = 1, nrpts
+      coeff_ik = EXP(ci * 2.d0 * pi * (kx * rvec(1, ir) + ky * rvec(2, ir)))
+      coeff_ik = coeff_ik / ndegen(ir)
+      oper_k = oper_k + oper_r(:,:,ir) * coeff_ik
+  ENDDO
+!--------------------------------------------------------------------------
+END SUBROUTINE k_operator
+!--------------------------------------------------------------------------
+!
+!--------------------------------------------------------------------------
+FUNCTION find_free_unit()
+!! Taken from QE/UtilXlib/find_free_unit.f90
   IMPLICIT NONE
   !
   INTEGER :: find_free_unit
@@ -161,5 +194,4 @@ CONTAINS
   ENDDO
   CALL io_error('find_free_unit(): free unit not found ?!?')
 END FUNCTION find_free_unit
-
 END MODULE comms
--- src/semiinf.f90
+++ src/semiinf.f90
@@ -1,176 +1,164 @@
 !------------------------------------------------------------------------
 PROGRAM semiinf
+!------------------------------------------------------------------------
+!! Main driver of the semiinf.x program
+!------------------------------------------------------------------------
   USE comms
   USE parameters
-  USE hamiltonian, ONLY : omega, kx, ky
   USE iter_bulk
   USE iter_slab
   USE postprocess_green
   !
   IMPLICIT NONE
   !
-  character(len=20) :: buffer_temp
-  integer :: ik, irec, ik_start, ik_end, il
-  real(DP), allocatable :: dos_s0(:), dos_b(:), dos_spn_s(:,:)
-  real(DP), allocatable :: dos_up(:), dos_dn(:), dos_spn_s_up(:,:), dos_spn_s_dn(:,:)
-  REAL(DP), allocatable :: dos_layer(:,:)
-  integer :: n_dos_layer ! this should later go to parameters.f90
-  n_dos_layer = 0
-
-  CALL mpi_setup()
-
-  ! ... read input file
+  INTEGER :: n_dos_layer = 0 ! FIXME: this should go to parameters.f90 as input parameter
+  INTEGER :: ik, irec, ik_start, ik_end, il
+  INTEGER :: iunsurf, iunbulk, iunsurfsx, iunsurfsy, iunsurfsz, iunlayer
+  REAL(DP), ALLOCATABLE :: dos_s0(:), dos_b(:), dos_spn_s(:,:)
+  REAL(DP), ALLOCATABLE :: dos_up(:), dos_dn(:), dos_spn_s_up(:,:), dos_spn_s_dn(:,:)
+  REAL(DP), ALLOCATABLE :: dos_layer(:,:)
+  !
+  CALL mp_setup()
+  IF (is_root) THEN
+    WRITE(*, '(1x,a)') "======================================================"
+    WRITE(*, '(1x,a)') '                 Welcome to semiinf.x !'
+  ENDIF
+  !
+  ! Read input file
   CALL GETARG(1, input_filename)
   input_filename = TRIM(input_filename)
   CALL param_read()
   IF (is_root) CALL param_write()
-
-  ! ... allocate array in modules
+  !
+  ! Allocate array in modules
   IF (isslab) CALL iter_slab_allocate()
-  ! IF ((.NOT. isslab) .AND. (.NOT. isfinite)) CALL iter_bulk_allocate()
   IF ((.NOT. isslab)) CALL iter_bulk_allocate()
   CALL hamiltonian_setup()
   CALL pp_green_setup()
-
-  ! IF (isfinite .and. isspin) THEN
-    ! ALLOCATE(dos_up(num_energy))
-    ! ALLOCATE(dos_dn(num_energy))
-    ! ALLOCATE(dos_s0(num_energy))
-    ! IF (isspin) ALLOCATE(dos_spn_s(num_energy,3))
-    ! IF (isspin) ALLOCATE(dos_spn_s_up(num_energy,3))
-    ! IF (isspin) ALLOCATE(dos_spn_s_dn(num_energy,3))
-    ! inquire (iolength = irec) dos_s0
-    ! open (unit = 8, file = "dos_surfup.out", form="unformatted", access="direct", recl=irec)
-    ! open (unit = 9, file = "dos_surfdn.out", form="unformatted", access="direct", recl=irec)
-    ! open (unit = 10, file = "dos_slab.out", form="unformatted", access="direct", recl=irec)
-    ! open (unit = 3, file = "dos_surf_sslab.out", form="unformatted", access="direct", recl=irec*3)
-    ! open (unit = 4, file = "dos_surf_sup.out", form="unformatted", access="direct", recl=irec*3)
-    ! open (unit = 5, file = "dos_surf_sdn.out", form="unformatted", access="direct", recl=irec*3)
-  ! ELSE
-    ALLOCATE(dos_s0(num_energy))
-    ALLOCATE(dos_b(num_energy))
-    IF (n_dos_layer>0) ALLOCATE(dos_layer(num_energy, n_dos_layer))
-    IF (isspin) ALLOCATE(dos_spn_s(num_energy,3))
-    inquire (iolength = irec) dos_s0
-    open (unit = 9, file = "dos_surf00.out", form="unformatted", access="direct", recl=irec)
-    inquire (iolength = irec) dos_b
-    open (unit = 10, file = "dos_bulk.out", form="unformatted", access="direct", recl=irec)
-    IF (isspin) open (unit = 3, file = "dos_surf_sx.out", form="unformatted", access="direct", recl=irec)
-    IF (isspin) open (unit = 4, file = "dos_surf_sy.out", form="unformatted", access="direct", recl=irec)
-    IF (isspin) open (unit = 5, file = "dos_surf_sz.out", form="unformatted", access="direct", recl=irec)
-    IF (n_dos_layer > 0) THEN
-      inquire (iolength = irec) dos_layer(:,:)
-      write(buffer_temp,'("dos_surf",I0.2,".out")'), 1
-      open (unit=11, file = buffer_temp, form="unformatted", access="direct", recl=irec)
-    END IF
-    ! DO il = 1, n_dos_layer
-      ! write(buffer_temp,'("dos_surf",I0.2,".out")'), il
-      ! open (unit=unit_il(il), file = buffer_temp, form="unformatted", access="direct", recl=irec)
-    ! END DO
-  ! END IF
-
-  CALL distribute_kpoint()
-  ! ... loop over k points
-  DO ik = ik_start, ik_end
-    CALL run_kpoint()
-  END DO
-  CLOSE(9)
-  CLOSE(10)
-  IF (isspin) CLOSE(3)
-  IF (isspin) CLOSE(4)
-  IF (isspin) CLOSE(5)
-  CLOSE(11)
-  ! DO il = 1, n_dos_layer
-    ! CLOSE(unit_il(il))
-  ! END DO
-
-  CALL mpi_end()
-
-CONTAINS
-SUBROUTINE distribute_kpoint()
-  integer :: ierr, an_id, nk_per_proc_quo, nk_per_proc_rem
-
-  nk_per_proc_quo = num_kpoint / num_procs
-  nk_per_proc_rem = num_kpoint - (num_procs*nk_per_proc_quo)
-  ! nk = q * (np-r) + (q+1) * r
-  write(*,"(1X,I,' or ',I,' k-points per process')") nk_per_proc_quo, nk_per_proc_quo+1
-  IF (my_id < (num_procs - nk_per_proc_rem)) THEN
-    ik_end = nk_per_proc_quo * (my_id + 1)
-    ik_start = ik_end - nk_per_proc_quo + 1
-  ELSE
-    ik_end = nk_per_proc_quo * (my_id + 1) + (my_id - num_procs + nk_per_proc_rem + 1)
-    ik_start = ik_end - nk_per_proc_quo
+  !
+  ALLOCATE(dos_s0(num_energy))
+  ALLOCATE(dos_b(num_energy))
+  IF (n_dos_layer > 0) ALLOCATE(dos_layer(num_energy, n_dos_layer))
+  IF (isspin) ALLOCATE(dos_spn_s(num_energy, 3))
+  !
+  ! Open units for binary file output
+  INQUIRE(IOLENGTH=irec) dos_s0
+  OPEN(UNIT=iunsurf, FILE="dos_surf00.out", FORM="unformatted", ACCESS="direct", RECL=irec)
+  INQUIRE(IOLENGTH=irec) dos_b
+  OPEN(UNIT=iunbulk, FILE="dos_bulk.out", FORM="unformatted", ACCESS="direct", RECL=irec)
+  IF (isspin) THEN
+    OPEN(UNIT=iunsurfsx, FILE="dos_surf_sx.out", FORM="unformatted", ACCESS="direct", RECL=irec)
+    OPEN(UNIT=iunsurfsy, FILE="dos_surf_sy.out", FORM="unformatted", ACCESS="direct", RECL=irec)
+    OPEN(UNIT=iunsurfsz, FILE="dos_surf_sz.out", FORM="unformatted", ACCESS="direct", RECL=irec)
   ENDIF
-  print*, "number of k point in node ", my_id, " is ", ik_end-ik_start+1
-END SUBROUTINE
-
-SUBROUTINE run_kpoint()
+  IF (n_dos_layer > 0) THEN
+    INQUIRE(IOLENGTH=irec) dos_layer(:,:)
+    OPEN(UNIT=iunlayer, FILE="dos_layer.out", FORM="unformatted", ACCESS="direct", RECL=irec)
+  ENDIF
+  !
+  CALL distribute_kpoint()
+  !
+  ! Loop over k points
+  DO ik = ik_start, ik_end
+    CALL run_kpoint(ik)
+  END DO
+  CLOSE(iunsurf)
+  CLOSE(iunbulk)
+  IF (isspin) THEN
+    CLOSE(iunsurfsx)
+    CLOSE(iunsurfsy)
+    CLOSE(iunsurfsz)
+  ENDIF
+  CLOSE(iunlayer)
+  !
+  CALL mp_barrier(world_comm)
+  IF (is_root) THEN
+    WRITE(*, '(1x,a)') 'Calculation complete: semiinf exiting'
+    WRITE(*, '(1x,a)') "======================================================"
+  ENDIF
+  !
+  CALL mp_end()
+  !
+CONTAINS
+!------------------------------------------------------------------------
+SUBROUTINE distribute_kpoint()
+!------------------------------------------------------------------------
+!! distribute k points to processors for k-point parallelization
+!------------------------------------------------------------------------
+  INTEGER :: q, r
+  ! nk = q * (np-r) + (q+1) * r
+  q = num_kpoint / num_procs
+  r = num_kpoint - (num_procs * q)
+  WRITE(*, "(X,'There are', I8,' or ',I8,' k-points per processor')") q, q+1
+  IF (my_id < (num_procs - r)) THEN
+    ik_end = q * (my_id + 1)
+    ik_start = ik_end - q + 1
+  ELSE
+    ik_end = q * (my_id + 1) + (my_id - num_procs + r + 1)
+    ik_start = ik_end - q
+  ENDIF
+  ! For debugging
+  ! PRINT*, "Number of k points in node", my_id, " is", ik_end - ik_start + 1
+!------------------------------------------------------------------------
+END SUBROUTINE distribute_kpoint
+!------------------------------------------------------------------------
+!
+!------------------------------------------------------------------------
+SUBROUTINE run_kpoint(ik)
+!------------------------------------------------------------------------
+!! Main driver of the calculation for each k point.
+!! For given kx and ky, calculate Green function and write DOS to file
+!------------------------------------------------------------------------
+  USE hamiltonian, ONLY : omega, kx, ky
   IMPLICIT NONE
+  INTEGER, INTENT(IN) :: ik
+  !! Index of the k point to be calculated
   INTEGER :: ie, il
-  ! ... set k-dependent hamiltonian
-  kx = plot_kpoint(1,ik)
-  ky = plot_kpoint(2,ik)
-  write(*,'("ik = ", I4, " kx = ", F6.3, " ky = ", F6.3)') ik, kx, ky
+  !
+  ! Set k-dependent hamiltonian
+  kx = plot_kpoint(1, ik)
+  ky = plot_kpoint(2, ik)
+  WRITE(*, '("ik = ", I4, " kx = ", F6.3, " ky = ", F6.3)') ik, kx, ky
   CALL hamiltonian_tb_to_k()
-  ! ... loop over energy points
+  ! Loop over energy points
   DO ie = 1, num_energy
     omega = energy(ie) * cone - sigma * ci
-    ! run iteration
+    ! Compute Green function by iteration
     IF (isslab) THEN
       CALL iter_slab_main()
-    ! ELSE IF (isfinite) THEN
-      ! CALL iter_slab_green_finite()
-      ! flag_converged = .true.
     ELSE
       CALL iter_bulk_main()
-    END IF
-    IF (.NOT. flag_converged) WRITE (*,'("DOS NOT converged, s=",ES8.1," N=",I2," ik=",I4," ie=",I5)') sigma, n_iter, ik, ie
-!        IF (MOD(ie,100)==0) WRITE (*,'("DOS converged, s=",ES8.1," N=",I2," ik=",I4," ie=",I5)') sigma, n_iter, ik, ie
-
-    ! run pp_green
-    ! IF (isfinite) THEN
-      ! CALL get_dos_s(dos_s0(ie))
-      ! CALL get_dos_up(dos_up(ie))
-      ! CALL get_dos_dn(dos_dn(ie))
-      ! IF (isspin) CALL get_spin_s(dos_spn_s(ie,:))
-      ! IF (isspin) CALL get_spin_up(dos_spn_s_up(ie,:))
-      ! IF (isspin) CALL get_spin_dn(dos_spn_s_dn(ie,:))
-    ! ELSE
-      CALL set_transfer_mat()
-      CALL get_dos_s(dos_s0(ie))
-      CALL get_dos_b(dos_b(ie))
-      IF (n_dos_layer > 0) CALL get_dos_nlayer(n_dos_layer, dos_layer(ie,:))
-      IF (isspin) CALL get_spin_s(dos_spn_s(ie,:))
-    ! END IF
+    ENDIF
+    IF (.NOT. flag_converged) &
+      WRITE(*, '("WARNING: DOS NOT converged, s=",ES8.1," N=",I2," ik=",I4," ie=",I5)') &
+        sigma, n_iter, ik, ie
+    !
+    ! Postprocess Green functions to calculate DOS, spin-DOS, etc.
+    CALL set_transfer_mat()
+    CALL get_dos_s(dos_s0(ie))
+    CALL get_dos_b(dos_b(ie))
+    IF (n_dos_layer > 0) CALL get_dos_nlayer(n_dos_layer, dos_layer(ie,:))
+    IF (isspin) CALL get_spin_s(dos_spn_s(ie,:))
+    !
     CALL pp_green_deallocate_green_layer()
+  END DO ! ie
+  !
+  ! Write DOS to file
+  WRITE(UNIT=iunsurf, REC=ik) dos_s0(:)
+  WRITE(UNIT=iunbulk, REC=ik) dos_b(:)
+  IF (isspin) WRITE(UNIT=iunsurfsx, REC=ik) dos_spn_s(:,1)
+  IF (isspin) WRITE(UNIT=iunsurfsy, REC=ik) dos_spn_s(:,2)
+  IF (isspin) WRITE(UNIT=iunsurfsz, REC=ik) dos_spn_s(:,3)
+  DO il = 1, n_dos_layer
+    ! WRITE(unit=unit_il(il), REC=ik) dos_layer(:,il)
+    WRITE(UNIT=11, REC=ik) dos_layer(:,:)
   END DO
-  ! IF (isfinite) THEN
-    ! WRITE(unit=10, rec=ik) dos_s0
-    ! WRITE(unit=8, rec=ik) dos_up
-    ! WRITE(unit=9, rec=ik) dos_dn
-    ! IF (isspin) WRITE(unit=3, rec=ik) dos_spn_s
-    ! IF (isspin) WRITE(unit=4, rec=ik) dos_spn_s_up
-    ! IF (isspin) WRITE(unit=5, rec=ik) dos_spn_s_dn
-  ! ELSE
-    WRITE(unit=9, rec=ik) dos_s0(:)
-    WRITE(unit=10, rec=ik) dos_b(:)
-    IF (isspin) WRITE(unit=3, rec=ik) dos_spn_s(:,1)
-    IF (isspin) WRITE(unit=4, rec=ik) dos_spn_s(:,2)
-    IF (isspin) WRITE(unit=5, rec=ik) dos_spn_s(:,3)
-    DO il = 1, n_dos_layer
-      ! WRITE(unit=unit_il(il), rec=ik) dos_layer(:,il)
-      WRITE(unit=11, rec=ik) dos_layer(:,:)
-    END DO
-  ! END IF
+  !
   WRITE(*,'("ik=",I6.5," done")') ik
-END SUBROUTINE
-
-! SUBROUTINE write_dos(unit, dos_list)
-!     ! USE kinds
-!     IMPLICIT NONE
-!     integer, INTENT(IN) :: unit, ik
-!     real(DP), dimension(:), INTENT(IN) :: dos_list
-!     WRITE(unit, rec=ik) dos_list
-! END SUBROUTINE
-end program semiinf
+!------------------------------------------------------------------------
+END SUBROUTINE run_kpoint
+!------------------------------------------------------------------------
+!
+!------------------------------------------------------------------------
+END PROGRAM semiinf
 !------------------------------------------------------------------------
